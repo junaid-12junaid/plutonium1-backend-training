c079b3435729dddbf4bd9ee9e88d130ca50686f1
junaid-12junaid <junaidullakhan619@gmail.com>
2022-08-27 15:53:06 +0530

    hjk

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,48 +4,48 @@ const productModel=require('../models/productModel')
 
 const createOrder= async function(req,res){
     let set=req.body
-    let userID=await userModel.findById(set.userId)
-    let productID=await productModel.findById(set.productId)
-    if(!set.userId){
+    let UserID=set.userId
+    let ProductID=set.productId
+    let FreeUser=req.headers.isfreeappuser
+    let userID=await userModel.findById(UserID)
+    let productID=await productModel.findById(ProductID)
+    if(!UserID){
         return res.send({data:"userID is required"})
     }else if(!userID){
         return res.send({data:"give the valid UserId"})
-    }else if(!set.productId){
+    }else if(!ProductID){
         return res.send({data:"productID is required"})
     }else if(!productID){
         return res.send({data:"Give the valid productID"})
-    }else if(set.isFreeAppUser!==true){
-        
     }
-    let createOrder=await orderModel.create(set)
-    res.send({data:createOrder})
-    
+
+    let priceV=productID.price
+    let userp=userID.balance
+    let orderp=set.amount
+    // console.log(orderp)
+    if(FreeUser=='false'){
+        if(userp>priceV){
+                let UserU=await userModel.findByIdAndUpdate({_id:UserID},{$inc:{balance:-priceV}},{set:true})
+                let ordert=await productModel.findById({_id:ProductID}).select({_id:1,price:1})
+                // console.log(ordert)
+                set.amount=ordert.price
+                let usere=await userModel.findByIdAndUpdate({_id:UserID},{$set:{isFreeAppUser:FreeUser}})
+                let ordere=await orderModel.create(set)
+                res.send(ordere)
+        }else{
+            res.send({data:"the balance is insuficient"})
+        }
+    }else if(FreeUser=='true'){
+        set.amount=0
+        let useru=await userModel.findByIdAndUpdate({_id:UserID},{$set:{isFreeAppUser:FreeUser}})
+        let ordere=await orderModel.create(set)
+                res.send(ordere)
+
+    }
 }
 
-// const orderValidation=async function(req,res,next){
-//     let orderf=await orderModel.find({isFreeAppUser:false}).populate(['userId','productId'])
-//     let ordere=await orderf.
-    // let 
-    // let fil=orderf.isFreeAppUser
-    // let fil1=orderf.userId
-    // let fil2=orderf.productId.price
-    // let fil4=orderf.amount
-    // if(fil==false){
-    //     let fil3=fil1-fil2
-    //     fil1=fil3
-    //     fil2=fil4
-    // }
-    // console.log(orderf)
 
 
 
 
-
-
-
-
-
-
- 
 module.exports.createOrder=createOrder
-//module.exports.orderValidation=orderValidation
--- src/middlewares/userMid.js
+++ src/middlewares/userMid.js
@@ -1,11 +1,9 @@
 const isFreeMid=function(req,res,next){
 
 let user=req.headers.isfreeappuser
-if(!user){
+if(!user)
     return res.send({data:"request is mandatory"})
-}else{
-    req.body["isFreeAppUser"]=Boolean(user)
-}
+
 next()
 }
 module.exports.isFreeMid=isFreeMid
