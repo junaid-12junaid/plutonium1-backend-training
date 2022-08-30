e9db8180ddb2a1a0069cfd15d730d27912035d47
junaid-12junaid <junaidullakhan619@gmail.com>
2022-08-30 19:45:18 +0530

    hjk

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,21 +2,35 @@ const userModel=require('../models/userModel')
 const jwt=require('jsonwebtoken')
 
 const userC=async function(req,res){
+    try{
+
     let set=req.body
+    if(Object.keys(set)!=0){
     let userc=await userModel.create(set)
     res.send({data:userc})
+    }else{
+        res.status(400).send({Data:"the user Input is empty"})
+    }
+}
+    catch(err){
+        console.log("this is the error:" ,err.message)
+        res.status(500).send({data:err.message})
+    }
 }
 
 
 const login=async function(req,res){
+    try{
+
     let emailId=req.body.emailId
     let password=req.body.password
-
-    let userC=await userModel.findOne({emailId:emailId,password:password})
-    if(!userC){
-        return res.send({alert:"the userId or password is Invalid"})
+    if(!emailId && !password){
+        res.status(401).send({data:"the emaid and password required"})
     }
 
+    let userC=await userModel.findOne({emailId:emailId,password:password})
+    if(userC){
+     
     let token=jwt.sign({
         UserID:userC._id.toString(),
         name:"This is Junaid",
@@ -25,48 +39,69 @@ const login=async function(req,res){
         "joind-At":"18-7-2022"
     },"hi-buddy-whatsapp-bro")
     res.setHeader('x-auth-token',token)
+    //res.header('x-auth-token',token)
     res.send({token:token})
     console.log(token)
+}
+    else{
+        return res.status(400).send({alert:"the userId or password is Invalid"})
 
+    }
+
+}catch(err){
+    res.status(500).send({Data:err.message})
+}
 }
 
 
-
 const userF=async function(req,res){
-
+try{
 let userId=req.params.userId
     let userf=await userModel.findById(userId)
-    if(!userf){
-        return res.send({alert:"no such user exists"})
+    if(userf){
+        res.send({data:userf})
+       
+    }else{
+        return res.status(404).send({alert:"no such user exists"})
     }
-res.send({data:userf})
+
+}catch(err){
+    res.status(500).send({Error:err.message})
+}
 }
 
 
 const userUp=async function(req,res){
+    try{
     let userId=req.params.userId
     let userR=await userModel.findById(userId).select({_id:1})
-    if(!userR){
-        return res.send({alert:"the user does't exists"})
+    if(userR){   
+        let set=req.body
+        let useru=await userModel.findOneAndUpdate({_id:userId},{$set:set},{new:true})
+        res.send({data:useru})
+      }else{
+        return res.status(404).send({alert:"the user does't exists"})
+      }
+ }catch(err){
+        res.status(500).send({data:err.message})
     }
-    let set=req.body
-    let useru=await userModel.findOneAndUpdate({_id:userId},{$set:set},{new:true})
-    res.send({data:useru})
-
-
 }
 
 const userD=async function(req,res){
+    try{
     let userId=req.params.userId
-    if(!userId){
-        return res.send({data:"the user does't exists"})
-    }
+    if(userId){
     let userDE=await userModel.findByIdAndUpdate({_id:userId},{$set:{isDeleted:true}},{new:true})
     res.send({data:userDE})
+    }else{
+        return res.status(404).send({data:"the user does't exists"})
+    }
+}
+    catch(err){
+        res.status(500).send({data:err})
+    }
 
 }
-
-
 
 module.exports.userC=userC
 module.exports.login=login
--- src/middleWare/userMiddleWare.js
+++ src/middleWare/userMiddleWare.js
@@ -1,23 +1,27 @@
 const jwt=require('jsonwebtoken')
 
 const mid1=async function(req,res,next){
+    try{
     let token=req.headers['x-Auth-Token']
     if(!token) {
         token=req.headers['x-auth-token']
     }
     if(!token){
-        return res.send({alert:"the token is mandatory"})
+        return res.status(401).send({alert:"the token is mandatory"})
     }
     let decode=jwt.verify(token,'hi-buddy-whatsapp-bro')
     if(!decode){
-        return res.send({status:false,data:"The token is invalid"})
+        return res.status(404).send({status:false,data:"The token is invalid"})
     }
     let logedUserId=decode.UserID
     let userId=req.params.userId
     if(userId!=logedUserId){
-        return res.send({status:false,data:"you can't access || update || delete data of other users"})
+        return res.status(403).send({status:false,data:"you can't access || update || delete data of other users"})
     }
     next()
+}catch(err){
+    res.status(500).send({Error:err.message})
+}
 }
 
 
